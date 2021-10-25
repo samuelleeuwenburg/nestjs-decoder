6644f3b6b3a8740d2a4cff30fbc6e69b48ad34c4
Wouter de Wild <wouter@codewild.nl>
2021-10-25 14:26:59 +0200

    Added Type checking and decoding of body

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Post,
+} from '@nestjs/common';
 import { Codec, GetType, number, string } from 'purify-ts/Codec';
 
 const User = Codec.interface({
@@ -8,16 +14,35 @@ const User = Codec.interface({
 
 type User = GetType<typeof User>;
 
+const Decode =
+  <T>(codec: Codec<T>) =>
+  (
+    target: any,
+    propertyKey: string,
+    descriptor: TypedPropertyDescriptor<Fn<T>>,
+  ) => {
+    const value = descriptor.value!;
+
+    (descriptor as any).value = function (argument: T) {
+      try {
+        return value.call(this, codec.unsafeDecode(argument));
+      } catch (error) {
+        throw new BadRequestException(error.message);
+      }
+    };
+  };
+
+type Fn<T> = (x: T) => void;
+
 @Controller('/user')
 export class AppController {
-  constructor() {}
-
   @Get()
   getHello(): User {
     return { username: 'foo', age: 60 };
   }
 
   @Post()
+  @Decode(User)
   sendUser(@Body() user: User) {
     return `hello ${user.username}, you are ${user.age} old \n`;
   }
